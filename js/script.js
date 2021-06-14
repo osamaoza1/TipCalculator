7d93dbd97c0019e7e3795192716290b29f9a7dc4
osamaoza1 <quickrazor07@aol.com>
2021-06-14 16:45:17 -0400

    Finished js so it would calculate the tip

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-const bill = document.getElementById('bill')
-bill.value = "100"
+const output = document.getElementById("output")
 
-document.getElementById("tip").innerHTML = "dfs"
+const percentButtons = document.querySelectorAll('#percent')
+const customButton = document.getElementById('customButton')
 
-document.getElementById("p1").innerHTML = "New text!"
+
+percentButtons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        let percentage = parseFloat(button.innerHTML) / 100
+        output.innerHTML = percentage * parseFloat(document.getElementById('bill').value)
+    })
+})
+
+customButton.addEventListener('click', ()=>{
+    let percentage = parseFloat(document.getElementById('customInput').value) / 100
+    output.innerHTML = percentage * parseFloat(document.getElementById('bill').value)
+})
